1848569bc21daebfb18f4facc31a9b125b683972
ejszymon <hd.kolodziejczak@gmail.com>
2015-12-08 18:02:18 +0100

    zmiana maina

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import interpreter.Interpreter;
+import interpreter.*;
+import modul1.*;
+import modul4.*;
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		Thread t1 = new Thread(new Interpreter());
 		t1.start();
-
+		hdd_commander driver = new hdd_commander(32,32);
 	}
 
 }
