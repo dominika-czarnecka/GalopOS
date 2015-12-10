051dd22e1a6c47ccc43f4768908392251609c641
dominika-czarnecka <domcia120595@wp.pl>
2015-12-10 17:02:54 +0100

    Wprowadzenie programów do pamięci

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,35 @@ import modul3.*;
 import modul4.*;
 
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		Thread t1 = new Thread(new Interpreter());
-		t1.start();
+		
 		hdd_commander driver = new hdd_commander(32,32);
-		Processor processor
+		
+		driver.create("prog1", "MVI C 10" + "\n"
+				+ "MVI B 1" + "\n"
+				+ "ADD A B" + "\n"
+				+ "INR B" + "\n"
+				+ "INR B" + "\n"
+				+ "DCR C" + "\n"
+				+ "JNZ 2" + "\n"
+				+ "HLT" + "\n");
+		
+		driver.create("prog2", "$JOB,20,CZYTNIK=*IN,DRUKARKA=*OUT"
+				+ "OFR prog2dane.txt" + "\n"
+				+ "RF A" + "\n"
+				+ "RF B" + "\n"
+				+ "ADD A B" + "\n"
+				+ "MOD2 C A" + "\n"
+				+ "CFR" + "\n"
+				+ "OFW prog2wynik.txt" + "\n"
+				+ "JNZ 11" + "\n"
+				+ "WF Suma_parzysta" + "\n"
+				+ "JZ 12" + "\n"
+				+ "WF Suma_nieparzysta" + "\n"
+				+ "CFW" + "\n"
+				+ "HLT"+ "\n");
 	}
 
 }
