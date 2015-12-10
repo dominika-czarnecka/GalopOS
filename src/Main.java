f372da90d7bd89f1be3ddbaabb0a242a381aa2e1
ejszymon <hd.kolodziejczak@gmail.com>
2015-12-10 17:05:19 +0100

    polka

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,17 @@ import modul4.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Thread t1 = new Thread(new Interpreter());
-		t1.start();
+
 		hdd_commander driver = new hdd_commander(32,32);
-		Processor processor
+		driver.create("prog1", 
+			   "tekst"
+		+"\n"+ "tekst2"
+		+"\n"+ "tekst3"
+		+"\n"+ "tekst4");
+		System.out.println(driver.read("prog1"));
+		driver.driver_show();
+		
+		
 	}
 
 }
